001f4151004351a0be59d5ea7f8169860a9455b4
Thailand2550-Chin <yoyofive4@gmail.com>
2024-07-12 14:59:00 +0700

    Add files via upload

---
+++ code2/index.js
@@ -0,0 +1,328 @@
+var product = [{
+    id: 1,
+    img:'https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'รองเท้า Nike รุ่นสีเเดง',
+    price: 2500,
+    description: 'Nike Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae rem necessitatibus exercitationem. Rem, nemo cupiditate.',
+    type:'shoe'
+}, {
+    id: 2,
+    img:'https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'รองเท้า Nike รุ่นพิเศษครบรอบ7ปี',
+    price: 3500,
+    description: 'Nike Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae rem necessitatibus exercitationem. Rem, nemo cupiditate.',
+    type: 'shoe'
+}, {
+    id: 3,
+    img:'https://images.unsplash.com/photo-1576566588028-4147f3842f27?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'เสื้อสายญี่ปุ่น',
+    price: 499,
+    description: 'เป็นเสื้อลายญี่ปุ่นคุณภาพดี',
+    type: 'clothing'
+}, {
+    id: 4,
+    img:'https://images.uncyc.org/th/1/1f/Abe555.jpeg',
+    name: 'อาเบะ',
+    price: 9999999,
+    description: 'ยาราไนก้า!!!!จะไปหาคุณรอได้เลย',
+    type: 'other'
+}, {
+    id: 5,
+    img:'https://pbs.twimg.com/media/CaGC9dqUkAAFhvP?format=jpg&name=large',
+    name: 'หุ่นยนต์แมวเถื่อนๆๆ',
+    price: 1500000,
+    description: 'รายละเอียด:เป็นหุ่นยนต์เเมวที่มาจากศตวรรษที่22ไร้ประโยชน์ชอบใช้ความรุนเเรงเเถมกินสุราทั้งวันไม่ยอมทำอะไรเลย',
+    type: 'novelty'
+}, {
+    id: 6,
+    img:'https://pbs.twimg.com/media/FFB1Bu8UYAM4a8-?format=jpg&name=large',
+    name: 'รูปปั้นพระอิศวร [ pasulol ]',
+    price: 650000,
+    description: 'ใครคืิอสุดยอดพระเจ้า!!!',
+    type: 'holy'
+}, {
+    id: 7,
+    img:'https://pbs.twimg.com/media/FtXvhr5aAAAPIam?format=jpg&name=900x900',
+    name: 'พระอิศวรเวอร์ชั่นผู้หญิง [ pasulol ]',
+    price: 650000,
+    description: 'อย่าน่ะ......',
+    type: 'holy'
+}, {
+    id: 8,
+    img:'https://poppyplaytime.com/cdn/shop/files/PoppyPlaytime_ScaryHuggyWuggyPlush_19in_1_Front.jpg',
+    name: 'ตุ๊กตาสุดน่ารัก [huggy wuggy]',
+    price: 1000,
+    description: 'ฉันอยากกอดคุณ',
+    type: 'toy'
+}, {
+    id: 9,
+    img:'https://s.isanook.com/mv/0/ud/8/42853/bh.jpg',
+    name: 'ตุ๊กตาสุดน่ารัก [เเอนนาเบล]',
+    price: 1500,
+    description: '????',
+    type: 'toy'
+}, {
+    id: 10,
+    img:'https://blog.hungryhub.com/wp-content/uploads/2022/06/3184724934_4587d3ac39_c-768x576.webp',
+    name: 'ไข่ข้าว',
+    price: 325,
+    description: 'ไข่ที่มีเชื้อผสมเรียบร้อยแล้วแต่ฟักไม่ออก มีทั้งแบบที่ไม่เป็นตัว และแบบที่มีตัวอ่อนฟักอยู่ภายในไข่',
+    type: 'food'
+}, {
+    id: 11,
+    img:'https://res.cloudinary.com/dk0z4ums3/image/upload/v1502332452/attached_image_th/%E0%B8%AA%E0%B9%89%E0%B8%A1%E0%B8%81%E0%B8%B1%E0%B8%9A%E0%B8%AA%E0%B8%A3%E0%B8%A3%E0%B8%9E%E0%B8%84%E0%B8%B8%E0%B8%93%E0%B8%97%E0%B8%B2%E0%B8%87%E0%B8%A2%E0%B8%B2-%E0%B9%81%E0%B8%84%E0%B9%88%E0%B8%84-pobpad.jpg',
+    name: 'ส้ม',
+    price: 45,
+    description: 'เป็นส้มที่เก็บจากข้างบ้านมีไม่เยอะรีบซื้อกันล่ะ',
+    type: 'food'
+}, {
+    id: 12,
+    img:'https://s3.ap-southeast-1.amazonaws.com/media.kawebook/img_story_cover/1694309374_76449569.jpg',
+    name: 'มังกร',
+    price: 32500000000,
+    description: 'ในตอนนี้เราสามารถนำมังกรเป็นสัตว์เลี้ยงได้เเล้ว',
+    type: 'other'
+}, {
+    id: 13,
+    img:'https://fq.lnwfile.com/_/fq/_raw/em/fc/g1.jpg',
+    name: 'ที่ห้อยกระเป๋าลาบู้บู้',
+    price: 225,
+    description: 'ของดี',
+    type: 'decorations'
+}, {
+    id: 14,
+    img:'https://shoplri.com/wp-content/uploads/2014/01/wpid-gnm_pic63.jpg',
+    name: 'ทองคำ 99.99% หนัก100บาท',
+    price: 4050000,
+    description: 'เป็นสิ่งที่มีค่าตลอดกาล',
+    type: 'decorations'
+}, {
+    id: 15,
+    img:'https://f.ptcdn.info/378/065/000/pvumtzubacT7j12mPM9-o.jpg',
+    name: 'ชุด Gold Saint ราศีเมษ',
+    price: 96500000,
+    description: 'เจ้านะ...อยากเป็นsaintของอาธีน่าไหมล่ะ?',
+    type: 'clothing'
+}, {
+    id: 16,
+    img:'https://us-fbcloud.net/wb/data/842/842490-topic-1420792392-3p.jpg',
+    name: 'เครื่องซักผ้าแบบติดข้อเท้า',
+    price: 11200,
+    description: 'สะดวกดีนะ',
+    type: 'electric'
+}, {
+    id: 17,
+    img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1qf1jZoF3zP7n8MxKUtDE7vdU_8T2CmQc6X5Q0XKxdg&s',
+    name: 'ริมุรุ',
+    price: 110000000,
+    description: 'สำหรับเลี้ยงสไลม์',
+    type: 'other'
+}, {
+    id: 18,
+    img:'https://f.ptcdn.info/454/053/000/ovt6vyzx0v6kN2SwI2i-o.jpg',
+    name: 'วิธีไปต่างโลก',
+    price: 312400,
+    description: 'เป็นวิธีที่ตัวละครในอนิเมะญี่ปุ่นส่วนมากมักใช้วิธีนี้ไปต่างโลก',
+    type: 'novelty'
+}, {
+    id: 19,
+    img:'https://png.pngtree.com/png-vector/20240129/ourlarge/pngtree-magic-potion-bottle-isolated-png-image_11583398.png',
+    name: 'โพชั่นฟื้นฟูมานาขั้นสูง',
+    price: 712340,
+    description: 'เหมาะกับนักเวทย์',
+    type: 'food'
+}, {
+    id: 20,
+    img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy3N7zSz-PGM7uvQX444zqomEw6IpSf-Prp-cZtHdDMw&s',
+    name: 'ยาละลายเเต่เสื้อผ้า',
+    price: 5600,
+    description: 'มันดูเหมาะกับคุณนะ555',
+    type: 'novelty'
+}, {
+    id: 21,
+    img:'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTGAho9dPtaFA-umv01yhbLznzDMwPn-zqhc0Iw5qXO4h6iHpwQ',
+    name: 'ยา???',
+    price: 41300,
+    description: 'เป็นยาที่ลูเดียสใช้กับซิลฟี',
+    type: 'novelty'
+}, {
+    id: 22,
+    img:'https://static.wikia.nocookie.net/villains/images/c/c8/C-Cat.jpg',
+    name: 'cantoon cat',
+    price: 666666,
+    description: 'ฉัน...จะ..ไป.หา..เเก..555555',
+    type: 'novelty'
+}];
+
+// [{},{},{}] // length = 3
+
+$(document).ready(() => {
+    var html = '';
+    for (let i = 0; i < product.length; i++) {
+        html += `<div onclick="openProductDetail(${i})" class="product-items ${product[i].type}">
+                    <img class="product-img" src="${product[i].img}" alt="">
+                    <p style="font-size: 1.2vw;">${product[i].name}</p>
+                    <p style="font-size: 1vw;">${numberWithCommas(product[i].price)} บาท</p>
+                </div>`;
+    }
+    $("#productlist").html(html);
+
+})
+
+function numberWithCommas(x) {
+    x = x.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x))
+        x = x.replace(pattern, "$1,$2");
+    return x;
+}
+
+function searchsomething(elem) {
+    console.log('#'+elem.id)
+    var value = $('#'+elem.id).val()
+    console.log(value)
+
+    var html = '';
+    for (let i = 0; i < product.length; i++) {
+        if( product[i].name.includes(value) ) {
+            html += `<div onclick="openProductDetail(${i})" class="product-items ${product[i].type}">
+                    <img class="product-img" src="${product[i].img}" alt="">
+                    <p style="font-size: 1.2vw;">${product[i].name}</p>
+                    <p style="font-size: 1vw;">${numberWithCommas(product[i].price)} บาท</p>
+               </div>`;
+        }
+    }
+    if(html == '') {
+        $("#productlist").html(`<p>ไม่พบสิ้นค้า</p>`);  
+    } else {
+        $("#productlist").html(html);
+    }
+
+}
+
+function searchproduct(param) {
+    console.log(param)
+    $(".product-items").css('display','none')
+    if(param == 'all') {
+        $(".product-items").css('display', 'block')
+    }
+    else {
+        $("."+param).css('display', 'block')
+    }
+}
+
+var productindex = 0;
+function openProductDetail(index) {
+    productindex = index;
+    console.log(productindex)
+    $("#modalDesc").css('display','flex')
+    $("#mdd-img").attr('src', product[index].img);
+    $("#mdd-name").text(product[index].name)
+    $("#mdd-price").text(numberWithCommas(product[index].price) + ' บาท ')
+    $("#mdd-desc").text(product[index].description)
+}
+
+function closeModal() {
+    $(".modal").css('display','none')
+}
+
+var cart = [];
+function addtocart() {
+    var pass = true;
+
+    for (let i = 0; i < cart.length; i++) {
+        if(productindex == cart[i].index) {
+           console.log('found same product')
+           cart[i].count++;
+           pass = false;
+        }
+    }
+
+    if(pass) {
+        var obj = {
+            index: productindex,
+            id: product[productindex].id,
+            name: product[productindex].name,
+            price: product[productindex].price,
+            img: product[productindex].img,
+            count: 1
+        };
+        console.log(obj)
+        cart.push(obj)
+    }
+    console.log(cart)
+
+    Swal.fire({
+        icon: 'success',
+        title: 'เพิ่ม ' + product[productindex].name + ' ไปในตะกร้าสำเร็จ !'
+    })
+    $("#cartcount").css('display','flex').text(cart.length)
+}
+function openCart() {
+    $('#modalCart').css('display','flex')
+    rendercart();
+}
+
+function rendercart() {
+    if(cart.length > 0) {
+        var html = '';
+        for (let i = 0; i < cart.length; i++) {
+            html += `<div class="cartlish-items">
+                        <div class="cartlist-left">
+                            <img src="${cart[i].img}" alt="">
+                            <div class="cartlist-detail">
+                                <p style="font-size: 1.5vw;">${cart[i].name}</p>
+                                <p style="font-size: 1.2vw;">${numberWithCommas(cart[i].price * cart[i].count)} บาท</p>
+                            </div>
+                        </div>
+                        <div class="cartlist-right">
+                            <p onclick="deinitems('-', ${i})" class="btnc">-</p>
+                            <p id="countitems${i}" style="margin: 0 20px;">${cart[i].count}</p>
+                            <p onclick="deinitems('+', ${i})" class="btnc">+</p>
+                        </div>
+                    </div>`;
+        }
+        $("#mycart").html(html)
+    }
+    else {
+        $("#mycart").html(`<p>ไม่มีรายการสิ้นค้าในตะกร้า</p>`)
+    }
+}
+function deinitems(action,index) {
+    if(action == '-') {
+        if(cart[index].count > 0) {
+            cart[index].count--;
+            $("#countitems"+index).text(cart[index].count)
+
+            if(cart[index].count <= 0) {
+                Swal.fire({
+                    icon: 'warning',
+                    title: 'คุณแน่ใจที่จะลบรายการนี้ใช่ไหม?',
+                    showConfirmButton: true,
+                    showCancelButton: true,
+                    confirmButtonText: 'ลบทิ้ง',
+                    cancelButtonText: 'ยกเลิก'
+                }).then((res) => {
+                    if(res.isConfirmed) {
+                        cart.splice(index, 1)
+                        console.log(cart)
+                        rendercart();
+                        $("#cartcount").css('display','flex').text(cart.length)
+
+                        if(cart.length <= 0) {
+                            $("#cartcount").css('display','none')
+                        }
+                    }
+                    else {
+                        cart[index].count++;
+                        $("#countitems"+index).text(cart[index].count)
+                    }
+                })
+            }
+        }
+    }
+    else if(action == '+') {
+        cart[index].count++;
+        $("#countitems"+index).text(cart[index].count)
+    }
+}
